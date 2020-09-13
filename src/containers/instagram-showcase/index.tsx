11e7af3798a86f499169bdb705c5d0f0f6348024
Tyler Newman <tylerjnewman18@gmail.com>
2020-09-13 11:15:43 -0700

    Remove insta

--- src/containers/instagram-showcase/index.tsx
+++ src/containers/instagram-showcase/index.tsx
@@ -10,60 +10,62 @@ import { IoLogoInstagram } from 'react-icons/io';
 
 interface ShowcaseProps {}
 
-const InstagramShowcase: React.FunctionComponent<ShowcaseProps> = () => {
-  const InstaData = useStaticQuery(graphql`
-    query {
-      allInstaNode(limit: 5) {
-        edges {
-          node {
-            id
-            likes
-            comments
-            localFile {
-              childImageSharp {
-                fluid(maxWidth: 350, maxHeight: 350, quality: 90) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
+// const InstagramShowcase: React.FunctionComponent<ShowcaseProps> = () => {
+//   const InstaData = useStaticQuery(graphql`
+//     query {
+//       allInstaNode(limit: 5) {
+//         edges {
+//           node {
+//             id
+//             likes
+//             comments
+//             localFile {
+//               childImageSharp {
+//                 fluid(maxWidth: 350, maxHeight: 350, quality: 90) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `);
 
-  const InstaPosts = InstaData.allInstaNode.edges;
+//   const InstaPosts = InstaData.allInstaNode.edges;
 
-  return (
-    <InstShowcaseWrapper>
-      <ShowcaseTitle>
-        <IoLogoInstagram
-          style={{
-            display: 'block',
-            marginRight: '12px',
-            fontSize: '20px',
-          }}
-        />
-        Me On Instagram
-      </ShowcaseTitle>
-      <InstashowcaseRow>
-        {InstaPosts && (
-          <>
-            {InstaPosts.map(({ node }: any) => (
-              <InstashowcaseCol key={node.id}>
-                <InstagramGrid
-                  image={node.localFile.childImageSharp.fluid}
-                  like={node.likes}
-                  comment={node.comments == null ? '0' : node.comments}
-                  url={`https://www.instagram.com/p/${node.id}`}
-                />
-              </InstashowcaseCol>
-            ))}
-          </>
-        )}
-      </InstashowcaseRow>
-    </InstShowcaseWrapper>
-  );
-};
+//   return (
+//     <InstShowcaseWrapper>
+//       <ShowcaseTitle>
+//         <IoLogoInstagram
+//           style={{
+//             display: 'block',
+//             marginRight: '12px',
+//             fontSize: '20px',
+//           }}
+//         />
+//         Me On Instagram
+//       </ShowcaseTitle>
+//       <InstashowcaseRow>
+//         {InstaPosts && (
+//           <>
+//             {InstaPosts.map(({ node }: any) => (
+//               <InstashowcaseCol key={node.id}>
+//                 <InstagramGrid
+//                   image={node.localFile.childImageSharp.fluid}
+//                   like={node.likes}
+//                   comment={node.comments == null ? '0' : node.comments}
+//                   url={`https://www.instagram.com/p/${node.id}`}
+//                 />
+//               </InstashowcaseCol>
+//             ))}
+//           </>
+//         )}
+//       </InstashowcaseRow>
+//     </InstShowcaseWrapper>
+//   );
+// };
 
-export default InstagramShowcase;
+// export default InstagramShowcase;
+
+export default () => {}
