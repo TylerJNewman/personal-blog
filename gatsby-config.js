2aee23b465e738e9c9792ea9f4b7d7cfda447b1f
Tyler Newman <tnewman@clairglobal.com>
2021-04-15 11:47:43 -0700

    Update Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Tyler Newman`,
+    title: `Personal Website`,
     author: `Tyler Newman`,
     about: `"Human perception is literally incarnation" ~ Marshall Mcluhan`,
     description: `A Gatsby Blog`,
